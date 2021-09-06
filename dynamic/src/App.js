7b6d4a653c7343634ca931b43c30fc26ce924bce
deepikaLG <62758134+deepikaLG@users.noreply.github.com>
2021-09-06 08:37:22 +0530

    project

--- dynamic/src/App.js
+++ dynamic/src/App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Component/Navbar';
-import Home from './Component/pages/Home';
-import Services from './Component/pages/Services';
-import Products from './Component/pages/Products';
-import SignUp from './Component/pages/SignUp';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-//import NewUser from './Component/pages/NewUser';
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './Component/NavBar';
+import Search from './Component/Search';
 
-function App() {
-  return (
-    <>
-    <Router>
-      <Navbar/>
-     <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/Services' component={Services} /> 
-        <Route path='/products' component={Products} />
-        <Route path='/sign-up' component={SignUp} />
-      </Switch>
-     </Router>
-   </>
-  );
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
